f6e3403a38e5c5bec1277ad9e2932d4551f5339c
samedamci <samedamci@disroot.org>
2021-08-02 00:19:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="basedbinpy",
+    packages=find_packages(include=["basedbinpy"]),
+    version="0.1",
+    description="Simple python library for basedbin pastebin-like service",
+    author="samedamci",
+    license="ISC",
+    install_requires=requirements
+)
