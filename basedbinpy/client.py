cc4ec914596ea70abae6cdcf42bbaa0c9ffccea9
samedamci <samedamci@disroot.org>
2021-08-02 00:03:10 +0200

    Remove get_paste parameters
    These parameters are useless in this project.

--- basedbinpy/client.py
+++ basedbinpy/client.py
@@ -1,7 +1,6 @@
 from basedbinpy.config import allowed_media_types
 from basedbinpy.exceptions import InvalidMimeType
 from collections import namedtuple
-from typing import Optional, Any
 from mimetypes import MimeTypes
 from requests import post, get
 import json
@@ -17,25 +16,17 @@ class Client:
     def __convert_dict_to_obj(self, name: str, dict_: dict) -> object:
         return namedtuple(name, dict_.keys())(*dict_.values())
 
-    def get_paste(
-        self,
-        paste_id: str,
-        file_format: str = "base64",
-        plain_text_output: bool = False,
-    ) -> object:
-        params = {"file_format": file_format, "plain_text_output": plain_text_output}
-        output = get(f"{self.url}/paste/{paste_id}", params).text
-        if not plain_text_output:
-            output = self.__convert_json_to_dict(output)
-        else:
-            output = output.decode("utf-8")
-        return self.__convert_dict_to_obj("Paste", output)
+    def get_paste(self, paste_id: str) -> object:
+        output = get(f"{self.url}/paste/{paste_id}").text
+        return self.__convert_dict_to_obj("Paste", self.__convert_json_to_dict(output))
 
     def upload_file(self, filename: str) -> object:
         mime_type = MimeTypes().guess_type(filename)[0]
         if mime_type in allowed_media_types:
             with open(filename, "rb") as file:
-                response = post(f"{self.url}/upload", files={"file": (filename, file, mime_type)})
+                response = post(
+                    f"{self.url}/upload", files={"file": (filename, file, mime_type)}
+                )
                 return response.text
         else:
             raise InvalidMimeType("File mime type is not accepted by server")
