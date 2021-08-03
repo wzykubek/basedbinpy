469b4f4d00b025856f7466f2367195a89551fec7
samedamci <samedamci@disroot.org>
2021-08-03 16:07:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,47 @@
 from setuptools import find_packages, setup
+from os import path
 
-with open("requirements.txt", "r") as f:
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "requirements.txt"), "r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
+
+with open(path.join(this_directory, "README.md"), "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+source_url = "https://github.com/samedamci/basedbinpy"
 
 setup(
     name="basedbinpy",
-    packages=find_packages(include=["basedbinpy"]),
-    version="0.2",
-    description="Simple python library for basedbin pastebin-like service",
+    version="0.2.1",
+    description="Simple python library for basedbin pastebin-like service.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="samedamci",
-    license="ISC",
-    install_requires=requirements
+    author_email="samedamci@disroot.org",
+    url=source_url,
+    project_urls={
+        "Source": source_url,
+        "Tracker": f"{source_url}/issues",
+    },
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+    keywords="basedbin library api client",
+    python_requires=">=3.6",
+    packages=find_packages(include=["basedbinpy"]),
 )
