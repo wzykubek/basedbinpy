8790cf4d4643da34c4b11f38608887ac77571518
samedamci <samedamci@disroot.org>
2021-08-05 17:12:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ source_url = "https://github.com/samedamci/basedbinpy"
 
 setup(
     name="basedbinpy",
-    version="0.4",
+    version="0.5",
     description="Simple python library for basedbin pastebin-like service.",
     long_description=long_description,
     long_description_content_type="text/markdown",
